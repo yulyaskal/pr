912b7d4b7dd90c01757f8f16792abbf325aa5390
skalenkoyup.24 <skalenkoyulya@yandex.ru>
2022-11-25 13:07:19 +0300

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import app.Application;
+import io.github.humbleui.jwm.App;
+
+public class Main {
+
+    public static void main(String[] args) {
+        App.start(Application::new);
+    }
+
+}
---
+++ src/main/java/app/Application.java
@@ -0,0 +1,12 @@
+package app;
+
+import io.github.humbleui.jwm.Event;
+
+import java.util.function.Consumer;
+
+public class Application implements Consumer<Event> {
+    @Override
+    public void accept(Event event) {
+
+    }
+}
